fedddc358bdea52735cb3b70858027702576bd9f
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-27 19:02:07 +0530

    added SwiperButtonNext

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,4 +1,5 @@
 import {
+  IonButton,
   IonContent,
   IonHeader,
   IonPage,
@@ -7,7 +8,7 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import "swiper/css";
 
 import "./Intro.css";
@@ -19,6 +20,11 @@ interface ContainerProps {
   onFinish: () => void;
 }
 
+const SwiperButtonNext = ({ children }: any) => {
+  const swiper = useSwiper();
+  return <IonButton onClick={() => swiper.slideNext()}>{children}</IonButton>;
+};
+
 const Intro: React.FC<ContainerProps> = ({ onFinish }) => {
   return (
     <Swiper>
@@ -27,12 +33,21 @@ const Intro: React.FC<ContainerProps> = ({ onFinish }) => {
         <IonText>
           <h3>Build Awesome Apps with Ionic UI Components!</h3>
         </IonText>
+        <SwiperButtonNext />
       </SwiperSlide>
       <SwiperSlide>
         <img src={IntroSVG2} alt="Intro2" />
+        <IonText>
+          <h3>Create Poweful Native Apps with Capacitor.</h3>
+        </IonText>
+        <SwiperButtonNext />
       </SwiperSlide>
       <SwiperSlide>
         <img src={IntroSVG3} alt="Intro3" />
+        <IonText>
+          <h3>Enjoy Learning to Code!</h3>
+        </IonText>
+        <IonButton onClick={() => onFinish()}>Finish</IonButton>
       </SwiperSlide>
     </Swiper>
   );
