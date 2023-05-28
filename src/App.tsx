e31d8630960069b77f05c64bcac6a5323fbf9912
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-28 19:16:30 +0530

    changed mode to 'ios'

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,9 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Menu from "./pages/Menu";
 
-setupIonicReact();
+setupIonicReact({
+  mode: "ios",
+});
 
 const App: React.FC = () => (
   <IonApp>
