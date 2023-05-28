e987cb5a1d6d2a0c6d3031d3728f0c91c7682d93
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-28 07:50:10 +0530

    added ion menu bar

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,7 +1,9 @@
 import {
   IonContent,
   IonHeader,
+  IonMenu,
   IonPage,
+  IonRouterOutlet,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
@@ -10,12 +12,16 @@ import React from "react";
 const Menu: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Page Title</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">UI goes here...</IonContent>
+      <IonMenu contentId="main">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">Menu Items</IonContent>
+      </IonMenu>
+
+      <IonRouterOutlet id="main"></IonRouterOutlet>
     </IonPage>
   );
 };
