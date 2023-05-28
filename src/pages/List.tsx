c68f65be7bfad1e0f7d2518b308c77f93e926e28
Nirmalya Nayak <n.nayak263@gmail.com>
2023-05-28 12:44:53 +0530

    added trash icon btn to clrear list

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,15 +1,36 @@
 import {
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
+  useIonViewWillEnter,
 } from "@ionic/react";
-import React from "react";
+import { trashBinOutline } from "ionicons/icons";
+import React, { useState } from "react";
 
 const List: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState<any[]>([]);
+
+  useIonViewWillEnter(async () => {
+    const users = await getUsers();
+    setUsers(users);
+    setLoading(false);
+  });
+
+  const getUsers = async () => {
+    const data = await fetch("https://randomuser.me/api?results=10");
+    const users = await data.json();
+    return users;
+  };
+
+  const clearList = () => {};
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,6 +39,11 @@ const List: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>List Page</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={clearList}>
+              <IonIcon slot="icon-only" icon={trashBinOutline} color="light" />
+            </IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">List UI</IonContent>
